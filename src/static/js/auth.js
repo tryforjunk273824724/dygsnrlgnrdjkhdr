f5c3213352f458a74b21f2a5126950d6d90b125b
tryforjunk273824724 <69701182+tryforjunk273824724@users.noreply.github.com>
2020-08-14 19:43:21 -0500

    Update auth.js

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,7 +1,7 @@
 var OKTA_SETTINGS = {
-  baseUrl: "https://dev-111464.oktapreview.com",
-  clientId: "0oaejf8gmll1TiDRz0h7",
-  issuer: "https://dev-111464.oktapreview.com/oauth2/default"
+  baseUrl: "https://dev-722378.okta.com",
+  clientId: "0oaq5zufdw1xZlSDL4x6",
+  issuer: "https://dev-722378.okta.com/oauth2/default"
 };
 
 var okta = new OktaSignIn({
